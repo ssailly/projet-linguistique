3b603ab1d607c30bd36931a2294937e5591e5f5b
SSailly <steven.sailly@gmail.com>
2022-03-30 16:47:31 +0200

    Fenêtre permettant de dessiner

--- src/main/java/fr/upcite/App.java
+++ src/main/java/fr/upcite/App.java
@@ -3,26 +3,44 @@ package fr.upcite;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hello World!");
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
+		Group root=new Group();
+		Scene scene=new Scene(root, 300, 300);
+
+		Path path=new Path();
+		path.setStrokeWidth(1);
+		path.setStroke(Color.BLACK);
+		
+		EventHandler<MouseEvent> mouseHandler=new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
+			public void handle(MouseEvent mouseEvent) {
+				if(mouseEvent.getEventType()==MouseEvent.MOUSE_PRESSED){
+					path.getElements().add(new MoveTo(mouseEvent.getX(), mouseEvent.getY()));
+				}
+				else if(mouseEvent.getEventType()==MouseEvent.MOUSE_DRAGGED){
+					path.getElements().add(new LineTo(mouseEvent.getX(), mouseEvent.getY()));
+				}
 			}
-		});			
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250));
+		};
+
+		scene.setOnMousePressed(mouseHandler);
+		scene.setOnMouseDragged(mouseHandler);
+
+		root.getChildren().add(path);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
