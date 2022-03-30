d3bc558b1019f7adfadceb9321b7c7b568b44531
SSailly <steven.sailly@gmail.com>
2022-03-30 18:54:17 +0200

    Partie "proposition" + ajustement du dessin

--- src/main/java/fr/upcite/App.java
+++ src/main/java/fr/upcite/App.java
@@ -1,13 +1,12 @@
 package fr.upcite;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
@@ -15,32 +14,50 @@ import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	private int firstStrokeOutside=-1, lastStrokeInside=-1;
+
 	@Override
 	public void start(Stage primaryStage) {
-		Group root=new Group();
-		Scene scene=new Scene(root, 300, 300);
+		Pane drawingPane=new Pane(), proposalsPane=new Pane();
+		drawingPane.setStyle("-fx-background-color: #B6B6B6");
+		drawingPane.setMinSize(200, 200);
+		drawingPane.setMaxSize(200, 200);
+		proposalsPane.setMinSize(200, 200);
+		proposalsPane.setMaxSize(200, 200);
+		proposalsPane.getChildren().add(new Label("Proposals"));
+		
+		BorderPane borderPane=new BorderPane();
+		borderPane.setLeft(drawingPane);
+		borderPane.setRight(proposalsPane);
+		Scene scene=new Scene(borderPane);
 
 		Path path=new Path();
 		path.setStrokeWidth(1);
 		path.setStroke(Color.BLACK);
-		
-		EventHandler<MouseEvent> mouseHandler=new EventHandler<MouseEvent>() {
+		drawingPane.setOnMousePressed(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
-				if(mouseEvent.getEventType()==MouseEvent.MOUSE_PRESSED){
-					path.getElements().add(new MoveTo(mouseEvent.getX(), mouseEvent.getY()));
-				}
-				else if(mouseEvent.getEventType()==MouseEvent.MOUSE_DRAGGED){
-					path.getElements().add(new LineTo(mouseEvent.getX(), mouseEvent.getY()));
-				}
+				firstStrokeOutside=-1;
+				path.getElements().add(new MoveTo(mouseEvent.getX(), mouseEvent.getY()));
+				System.out.println(lastStrokeInside+", "+firstStrokeOutside);
 			}
-		};
+		});
+		drawingPane.setOnMouseDragged(new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				double x=mouseEvent.getX(), y=mouseEvent.getY();
+				boolean inDrawingPane=x<drawingPane.getWidth() && y<drawingPane.getHeight();
+				if(inDrawingPane && lastStrokeInside==-1) {
+					path.getElements().add(new LineTo(x, y));
+					firstStrokeOutside++;
+				}
+				else lastStrokeInside=firstStrokeOutside;
+			}
+		});
 
-		scene.setOnMousePressed(mouseHandler);
-		scene.setOnMouseDragged(mouseHandler);
-
-		root.getChildren().add(path);
+		drawingPane.getChildren().add(path);
 		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
 
