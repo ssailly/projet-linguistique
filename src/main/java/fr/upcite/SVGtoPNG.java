04647f6415a92ba4c9377a032301bb5c433ead5c
SSailly <steven.sailly@gmail.com>
2022-04-02 19:00:43 +0200

    Création des SVG modifiés

--- src/main/java/fr/upcite/SVGtoPNG.java
+++ src/main/java/fr/upcite/SVGtoPNG.java
@@ -1,16 +1,30 @@
 package fr.upcite;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class SVGtoPNG {
+	/**
+	 * Exécuter depuis la racine du projet
+	 */
 	private static void createUsableKanji(){
-		String pathDir=Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/kanji_svg_src";//depuis la racine
-		File dir=new File(pathDir);
-		for(File file:dir.listFiles()){
-			removeStrokeNumbers(file.getAbsolutePath());
-			//TODO Créer les SVG à partir du nouveau code utilisable
+		String pathSrcDir=Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/kanji_svg_src";
+		String pathTrgDir=Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/kanji_svg_trg";
+		File srcDir=new File(pathSrcDir);
+		File trgDir=new File(pathTrgDir);
+		if(!trgDir.mkdir()){
+			catchException(pathTrgDir, new IOException());
+			return;
+		}
+		for(File file:srcDir.listFiles()){
+			String newSvgCode=removeStrokeNumbers(file.getAbsolutePath());
+			createSvg(newSvgCode, pathTrgDir+"/"+file.getName());
+			//TODO conversion SVG -> PNG
 		}
 	}
 
@@ -30,6 +44,19 @@ public class SVGtoPNG {
 			catchException(path, e);
 		}
 		return(s);
+	}
+
+	private static void createSvg(String svgCode, String path){
+		File file=new File(path);
+		try {
+			if(file.createNewFile()){
+				FileWriter writer=new FileWriter(file);
+				writer.write(svgCode);
+				writer.close();
+			}
+		} catch (Exception e) {
+			catchException(path, e);
+		}
 	}
 
 	private static void catchException(String path, Exception e){
