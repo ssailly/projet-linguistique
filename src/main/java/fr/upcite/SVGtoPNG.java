2061fa053e8e220418829b1445244dbe889c13a4
SSailly <steven.sailly@gmail.com>
2022-04-06 19:43:03 +0200

    Conversion SVG -> PNG

--- src/main/java/fr/upcite/SVGtoPNG.java
+++ src/main/java/fr/upcite/SVGtoPNG.java
@@ -1,7 +1,14 @@
 package fr.upcite;
 
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.OutputStream; 
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -10,22 +17,20 @@ public class SVGtoPNG {
 	/**
 	 * Exécuter depuis la racine du projet
 	 */
-	private static void createPng(){
-		
-	}
-
-
 	private static void createUsableKanji(){
 		String pathSrcDir=Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/kanji_svg_src";
 		String pathTrgDir=Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/kanji_svg_trg";
+		String pathPngDir=Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/kanji_png";
 		System.out.println(pathSrcDir);
 		File srcDir=new File(pathSrcDir);
 		File trgDir=new File(pathTrgDir);
 		trgDir.mkdir();
 		for(File file:srcDir.listFiles()){
 			String newSvgCode=removeStrokeNumbers(file.getAbsolutePath());
-			createSvg(newSvgCode, pathTrgDir+"/"+file.getName());
-			//TODO conversion SVG -> PNG
+			String newSvgPath=pathTrgDir+"/"+file.getName();
+			String pngPath=pathPngDir+"/"+file.getName().substring(0, file.getName().length()-3)+"svg";
+			createSvg(newSvgCode, newSvgPath);
+			svgToPng(newSvgPath, pngPath);
 		}
 	}
 
@@ -60,6 +65,20 @@ public class SVGtoPNG {
 		}
 	}
 
+	private static void svgToPng(String svgPath, String pngPath){
+		try{
+			TranscoderInput input_svg_image = new TranscoderInput(new FileInputStream(svgPath));
+			OutputStream png_ostream = new FileOutputStream(pngPath);
+			TranscoderOutput output_png_image = new TranscoderOutput(png_ostream);
+			PNGTranscoder my_converter = new PNGTranscoder();
+			my_converter.transcode(input_svg_image, output_png_image);
+			png_ostream.flush();
+			png_ostream.close();
+		} catch(Exception e){
+			catchException(svgPath, e);
+		}
+	}
+	
 	private static void catchException(String path, Exception e){
 		System.err.println("Path: "+path);
 		e.printStackTrace();
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module fr.upcite {
-    requires javafx.controls;
-    requires javafx.swing;
-    requires javafx.fxml;
-    requires transitive java.desktop;
-
-    opens fr.upcite to javafx.fxml;
-    exports fr.upcite;
-}
