cc08af4cc75fed90001842598b7adf18155ada94
SSailly <steven.sailly@gmail.com>
2022-04-06 21:11:08 +0200

    Bouton clear

--- src/main/java/fr/upcite/App.java
+++ src/main/java/fr/upcite/App.java
@@ -2,7 +2,6 @@ package fr.upcite;
 
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.geom.AffineTransform;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,22 +12,26 @@ import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
 import javafx.stage.Stage;
 
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
 public class App extends Application {
@@ -37,25 +40,32 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		Pane drawingPane=new Pane(), proposalsPane=new Pane();
-		drawingPane.setStyle("-fx-background-color: #B6B6B6");
+		Pane drawingPane=new Pane();
+		BorderPane proposalsPane=new BorderPane();
 		drawingPane.setMinSize(200, 200);
 		drawingPane.setMaxSize(200, 200);
-		proposalsPane.setMinSize(200, 200);
-		proposalsPane.setMaxSize(200, 200);
-		proposalsPane.getChildren().add(new Label("Proposals"));
-		
-		BorderPane borderPane=new BorderPane();
-		borderPane.setLeft(drawingPane);
-		borderPane.setRight(proposalsPane);
-		Scene scene=new Scene(borderPane);
 		drawingPane.setStyle("-fx-background-color: transparent; ");
 		
+		proposalsPane.setMinSize(200, 200);
+		proposalsPane.setMaxSize(200, 200);
+		proposalsPane.setTop(new Label("Proposals"));
 
 		Path path=new Path();
 		//path.setStrokeWidth(1);
 		path.setStroke(Color.BLACK);
 		//path.setOpacity(1);
+
+		Button clear=new Button("Clear");
+		clear.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				path.getElements().clear();
+				saveImage(drawingPane);
+			}
+		});
+
+		proposalsPane.setBottom(new HBox(clear));
+		
 		drawingPane.setOnMousePressed(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
@@ -73,28 +83,35 @@ public class App extends Application {
 					firstStrokeOutside++;
 				}
 				else lastStrokeInside=firstStrokeOutside;
-				//save image
-				SnapshotParameters sp=new SnapshotParameters();
-				sp.setFill(Color.TRANSPARENT);
-				WritableImage img = drawingPane.snapshot(sp, null);
-				BufferedImage img2 = SwingFXUtils.fromFXImage(img, null);
-				BufferedImage img3 = new BufferedImage(IMG_SIZE, IMG_SIZE, img2.getType());
-				Graphics2D g = img3.createGraphics();
-				g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-				g.drawImage(img2, 0, 0, IMG_SIZE, IMG_SIZE, 0, 0, img2.getWidth(), img2.getHeight(), null);
-				g.dispose();
-				try {
-					ImageIO.write(img3, "png", new File(Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/image_output/image.png"));
-				} catch (IOException ex) {
-					//Logger.getLogger(GuiClass.class.getName()).log(Level.SEVERE, null, ex);
-				}
+				saveImage(drawingPane);
 			}
 		});
-
 		drawingPane.getChildren().add(path);
+
+		BorderPane borderPane=new BorderPane();
+		borderPane.setLeft(drawingPane);
+		borderPane.setRight(proposalsPane);
+		Scene scene=new Scene(borderPane);
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
+	}
+
+	private static void saveImage(Pane drawingPane){
+		SnapshotParameters sp=new SnapshotParameters();
+		sp.setFill(Color.TRANSPARENT);
+		WritableImage img = drawingPane.snapshot(sp, null);
+		BufferedImage img2 = SwingFXUtils.fromFXImage(img, null);
+		BufferedImage img3 = new BufferedImage(IMG_SIZE, IMG_SIZE, img2.getType());
+		Graphics2D g = img3.createGraphics();
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g.drawImage(img2, 0, 0, IMG_SIZE, IMG_SIZE, 0, 0, img2.getWidth(), img2.getHeight(), null);
+		g.dispose();
+		try {
+			ImageIO.write(img3, "png", new File(Paths.get(".").toAbsolutePath().normalize().toString()+"/resources/image_output/image.png"));
+		} catch (IOException ex) {
+			//Logger.getLogger(GuiClass.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 	public static void main(String[] args) {
